c83e605e5825e48dd45444c71260992cfe6157e9
farkhadtut <farkhadnavoi@yandex.com>
2026-01-05 00:25:52 +0500

    test: Change AppBar title to verify change propagation

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HurryFoods'),
+        title: const Text('HURRYFOODS_TEST'),
         actions: [
           IconButton(
             icon: const Icon(Icons.search),
